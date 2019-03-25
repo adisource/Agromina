fcce290919997bdc5843ae088f125e95ac218b51
FaisalHDT <faisalhdtbsm1@gmail.com>
2019-03-25 21:21:05 +0800

    first commit

--- cart.php
+++ cart.php
@@ -2,135 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cart</title>
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500">
-    <link rel="stylesheet" href="styles/style.css">
-    <script src="scripts/uikit.js"></script>
-    <script src="scripts/uikit-icons.js"></script>
-    <script src="scripts/svg-with-js/js/fontawesome-all.js"></script>
+<?php include "templates/component/head.php"?>
 </head>
 
 <body>
     <div class="uk-offcanvas-content">
             <header>
-                    <div class="uk-navbar-container uk-light uk-visible@m tm-toolbar-container">
-                        <div class="uk-container" uk-navbar>
-                            <div class="uk-navbar-left">
-                                <nav>
-                                    <ul class="uk-navbar-nav">
-                                        <li><a href="#"><span class="uk-margin-xsmall-right"
-                                                    uk-icon="icon: receiver; ratio: .75;"></span><span
-                                                    class="tm-pseudo">+6287881194047 </span></a></li>
-                                        <li><a href="contacts.php" onclick="return false"><span class="uk-margin-xsmall-right"
-                                                    uk-icon="icon: location; ratio: .75;"></span><span class="tm-pseudo">Dasan
-                                                    sawe, RT 001/ RW 001, Kel/desa.Banjarsari, kec. Labuhan Haji</span><span
-                                                    uk-icon="icon: triangle-down; ratio: .75;"></span></a>
-                                            <div class="uk-margin-remove" uk-drop="mode: click; pos: bottom-center;">
-                                                <div
-                                                    class="uk-card uk-card-default uk-card-small uk-box-shadow-xlarge uk-overflow-hidden uk-padding-small uk-padding-remove-horizontal uk-padding-remove-bottom">
-                                                    <figure class="uk-card-media-top uk-height-small js-map"
-                                                        data-latitude="59.9356728" data-longitude="30.3258604" data-zoom="14">
-                                                    </figure>
-                                                    <div class="uk-card-body">
-                                                        <div class="uk-text-small">
-                                                            <div class="uk-text-bolder">Agromina.id</div>
-                                                            <div>Dasan&nbsp; sawe,&nbsp;RT 001/ RW
-                                                                001,&nbsp;Kel/desa.Banjarsari,&nbsp;kec. Labuhan &nbsp;Haji
-                                                            </div>
-                                                            <div>Melayani 24 jam</div>
-                                                        </div>
-                                                        <div class="uk-margin-small"><a
-                                                                class="uk-link-muted uk-text-uppercase tm-link-to-all uk-link-reset"
-                                                                href="contacts.php"><span>Kontak</span><span
-                                                                    uk-icon="icon: chevron-right; ratio: .75;"></span></a></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="uk-navbar-item"><span class="uk-margin-xsmall-right"
-                                                    uk-icon="icon: clock; ratio: .75;"></span>Melayani
-                                                24 jam</div>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <div class="uk-navbar-right">
-                                <nav>
-                                    <ul class="uk-navbar-nav">
-                                        <li><a href="news.php">News</a></li>
-                                        <li><a href="faq.php">FAQ</a></li>
-                                        <li><a href="#">Payment</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="uk-navbar-container tm-navbar-container" uk-sticky="cls-active: tm-navbar-container-fixed">
-                        <div class="uk-container" uk-navbar>
-                            <div class="uk-navbar-left"><button class="uk-navbar-toggle uk-hidden@m"
-                                    uk-toggle="target: #nav-offcanvas" uk-navbar-toggle-icon></button><a
-                                    class="uk-navbar-item uk-logo" href="index.php"><img src="images/logo.png" width="90"
-                                        height="32" alt="Logo"></a>
-                                <nav class="uk-visible@m">
-                                    <ul class="uk-navbar-nav">
-                                        <li><a href="index.php"><i class="fas fa-home"></i>&nbsp;&nbsp;Beranda</a>
-                                        </li>
-                                        <li><a href="produk.php"><i class="fas fa-shopping-bag "></i>&nbsp;&nbsp;Produk</a>
-        
-                                        </li>
-                                        <li><a href="#"><i class="fas fa-gift" aria-hidden="true"></i>&nbsp;&nbsp;Pesan
-                                                Paket</a>
-        
-                                        </li>
-                                        <li><a href="cerita_dan_kegiatan.php"><i class="fas fa-newspaper"
-                                                    aria-hidden="true"></i>&nbsp;&nbsp;Cerita
-                                                & Kegiatan</a></li>
-                                        <li><a href="about.php">About</a></li>
-        
-                                    </ul>
-                                </nav>
-                            </div>
-                            <div class="uk-navbar-right"><a class="uk-navbar-toggle tm-navbar-button" href="#"
-                                    uk-search-icon></a>
-                                <div class="uk-navbar-dropdown uk-padding-small uk-margin-remove"
-                                    uk-drop="mode: click;cls-drop: uk-navbar-dropdown;boundary: .tm-navbar-container;boundary-align: true;pos: bottom-justify;flip: x">
-                                    <div class="uk-container">
-                                        <div class="uk-grid-small uk-flex-middle" uk-grid>
-                                            <div class="uk-width-expand">
-                                                <form class="uk-search uk-search-navbar uk-width-1-1"><input
-                                                        class="uk-search-input" type="search" placeholder="Search…" autofocus>
-                                                </form>
-                                            </div>
-                                            <div class="uk-width-auto"><a class="uk-navbar-dropdown-close" href="#"
-                                                    uk-close></a></div>
-                                        </div>
-                                    </div>
-                                </div><a class="uk-navbar-item uk-link-muted uk-visible@m tm-navbar-button"
-                                    href="compare.php"><i class="fas fa-heart "></i><span class="uk-badge">3</span></a><a
-                                    class="uk-navbar-item uk-link-muted tm-navbar-button" href="account.php"><i
-                                        class="fas fa-user"></i></a>
-                                <div class="uk-padding-small uk-margin-remove"
-                                    uk-dropdown="pos: bottom-right; offset: -10; delay-hide: 200;" style="min-width: 150px;">
-                                    <ul class="uk-nav uk-dropdown-nav">
-                                        <li><a href="account.php">Pesanan
-                                                <span>(2)</span></a></li>
-                                        <li><a href="favorites.php">Favorites
-                                                <span>(3)</span></a></li>
-                                        <li><a href="personal.php">Personal</a></li>
-                                        <li><a href="settings.php">Settings</a></li>
-                                        <li class="uk-nav-divider"></li>
-                                        <li><a href="#">Keluar</a></li>
-                                    </ul>
-                                </div><a class="uk-navbar-item uk-link-muted tm-navbar-button" href="cart.php"
-                                    uk-toggle="target: #cart-offcanvas" onclick="return false"><i
-                                        class="fas fa-shopping-cart"></i><span class="uk-badge">2</span></a>
-                            </div>
-                        </div>
-                    </div>
+            <?php include "templates/component/navbar.php"?>
                 </header>
         <main>
             <section class="uk-section uk-section-small">
